664843102d89bb506f481025a91efa08f64fbdb5
sandranymark <sandra@nymark.nu>
2024-02-15 12:14:32 +0100

    errormsg

--- script.js
+++ script.js
@@ -4,16 +4,26 @@ async function fetchData() {
     try {
 
         const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+
+
+
+        const response = await fetch(`https:pokeapi.co/api/v2/pokemon/${pokemonName}`)
 
         if (!response.ok) {
-            throw new error("KAN INTE HITTA");
+            throw new Error("KAN INTE HITTA");
         }
-
 
         const data = await response.json();
         const pokemonSprite = data.sprites.front_default;
         const imgElement = document.getElementById("pokemonSprite")
+
+
+
+        const markup = `<li>${data.name}</li>`;
+        document.querySelector('ul').insertAdjacentHTML('beforeend', markup);
+
+
+
 
         imgElement.src = pokemonSprite;
         imgElement.style.display = "block";
@@ -26,46 +36,7 @@ async function fetchData() {
         const errorMessageElement = document.getElementById("errorMessage");
         errorMessageElement.textContent = "Pokémon med det namnet existerar inte.";
         errorMessageElement.style.display = "block";
-
     }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Promise... NÄR VAR HUR VARFÖR?
-
-// fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-// .then(response => {
-
-//     if(!response.ok){
-//         throw new Error ("Kunde inte hitta data")
-//     }
-//     return response.json();
-// })
-// .then (data => console.log(data))
-// .catch(error => console.error(error))
